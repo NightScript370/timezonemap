4570f85d5cc209da860549c4a2c0398cddeaf82e
NightScript <18664762+NightScript370@users.noreply.github.com>
2025-09-28 20:03:33 -0400

    Update to v4.6

--- timezonemap/src/test/java/us/dustinj/timezonemap/TimeZoneMapTest.java
+++ timezonemap/src/test/java/us/dustinj/timezonemap/TimeZoneMapTest.java
@@ -349,7 +349,7 @@ public class TimeZoneMapTest {
 
     @Test
     public void getMapVersion() {
-        assertThat(EVERYWHERE.getMapVersion()).isEqualTo("4.5:2020d");
+        assertThat(EVERYWHERE.getMapVersion()).isEqualTo("4.6:2025b");
     }
 
     @Test
